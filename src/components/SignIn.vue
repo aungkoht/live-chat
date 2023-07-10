bda75f5a69d70403797bef775d5e885a15d116ec
aungkoht <aungkohtet2003@gmail.com>
2023-07-10 20:12:49 +0630

    sign in/up form

---
+++ src/components/SignIn.vue
@@ -0,0 +1,42 @@
+<template>
+   <h3>Log in to your account</h3>
+      <form >
+         <input type="email" placeholder="email">
+         <input type="password" placeholder="password">
+         <button>Confirm</button>
+      </form>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+   .welcome{
+      text-align: center;
+      padding:20px 0;
+   }
+
+   .welcome form{
+      max-width:300px;
+      margin:20px auto;
+   }
+   .welcome input{
+      display:block;
+      width:100%;
+      margin:20px auto;
+      padding:10px 0;
+      border:none;
+      border-bottom:1px solid #aaa;
+      outline:none;
+     
+   }
+   .welcome span{
+      color:#1fce6e;
+      text-decoration: underline;
+      cursor:pointer;
+   }
+</style>
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container welcome">
+      <div v-if="showForm">
+         <SignUp></SignUp>
+         <p>Do you have an account? <span @click = "showForm =! showForm">Log in </span></p>
+      </div>
+      <div v-else>
+         <SignIn></SignIn>
+         <p>Are you new user? <span @click=" showForm =! showForm">Create account</span></p>
+      </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import SignIn from '../components/SignIn'
+import SignUp from '../components/SignUp'
+export default {
+  components: {
+      SignIn, SignUp
+   },
+
+   setup() {
+     let  showForm = ref(true)
+
+      return{showForm}
+   }
+   
+}
+</script>
+
+<style>
+
+</style>
